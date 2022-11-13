bad24d6b64430759d064d38e16babe05be189947
Cold <kragevisdom@hotmail.com>
2022-11-13 03:24:05 -0600

    Aca ya esta el git
    Prepared

---
+++ Source/RogueLikeColdgame/RogueLikeColdgameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RogueLikeColdgameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ROGUELIKECOLDGAME_API ARogueLikeColdgameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
