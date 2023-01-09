8b3adbbb1ddfad6e13e00829bc1abb3a2e1fb4a6
Alberto Ortega <Kragevisdom@hotmail.com>
2023-01-09 02:21:12 -0600

    Acabado 5, sigue el 6
    Se hizo el movimiento adelante, atras y raton horizontal

--- Source/RogueLikeColdgame/Private/SCharacter.cpp
+++ Source/RogueLikeColdgame/Private/SCharacter.cpp
@@ -28,6 +28,11 @@ void ASCharacter::BeginPlay()
 	
 }
 
+void ASCharacter::MoveForward(float value)
+{
+	AddMovementInput(GetActorForwardVector(), value);
+}
+
 // Called every frame
 void ASCharacter::Tick(float DeltaTime)
 {
@@ -39,6 +44,10 @@ void ASCharacter::Tick(float DeltaTime)
 void ASCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAxis("MoveForward", this, &ASCharacter::MoveForward);
+
+	PlayerInputComponent->BindAxis("Turn", this, &APawn::AddControllerYawInput);
 
 }
 
